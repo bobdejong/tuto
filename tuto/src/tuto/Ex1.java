881ac5e907984b8d48ec911c4aa84115fee1b84e
x551 <x551@asus>
2018-06-06 20:26:59 +0900

    hello

---
+++ tuto/src/tuto/Ex1.java
@@ -0,0 +1,27 @@
+package tuto;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		 
+		int a,b;
+		int result;
+		
+		
+		a=2;
+				b=3;
+		
+				
+				result=a+b;
+		for (int i = 0; i <3; i++) {
+			
+			System.out.println(i);
+			
+			
+				
+		System.out.println(result);
+		System.out.println(i);
+		}
+	}
+
+}
---
+++ tuto/src/tuto/main.java
@@ -0,0 +1,10 @@
+package tuto;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("¾È³ç");
+	}
+
+}
